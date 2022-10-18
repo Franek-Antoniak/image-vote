ef0a7b7b56318331519efcbf9c961aac7bc308dc
Franek Antoniak <franekant123@gmail.com>
2022-10-18 23:21:20 +0200

    FIX: secure fixes
    - first attempt to hide the secret keys

--- src/main/java/com/highschool/image/vote/ImageVoteApplication.java
+++ src/main/java/com/highschool/image/vote/ImageVoteApplication.java
@@ -1,13 +1,14 @@
 package com.highschool.image.vote;
 
+import com.highschool.image.vote.config.GoogleAPIKey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImageVoteApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
+		GoogleAPIKey.setSecretCredentials(args[0], args[1]);
 		SpringApplication.run(ImageVoteApplication.class, args);
 	}
-
 }
